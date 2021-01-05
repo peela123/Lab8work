6f0068d939509ae318e27ba38b4db79cfd0da67b
peela123 <peelaxv@gmail.com>
2021-01-05 17:47:08 +0700

    งานlab8

---
+++ lab8_1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <iomanip> //For using setw(), setprecision(), ...
+using namespace std;
+
+int main()
+{
+	double loan, payment, interest_rate, interest;
+	cout << "Enter initial loan: ";
+	cin >> loan;
+	cout << "Enter interest rate per year (%): ";
+	cin >> interest_rate;
+	cout << "Enter amount you can pay per year: ";
+	cin >> payment;
+
+	//use 'setw' to set width of table and 'left' to set left-alignment
+	//you can change input argument of 'setw()' to see the effect
+	//Try to change from 'left' to 'right' and see the effect
+	cout << setw(13) << left << "EndOfYear#";
+	cout << setw(13) << left << "PrevBalance";
+	cout << setw(13) << left << "Interest";
+	cout << setw(13) << left << "Total";
+	cout << setw(13) << left << "Payment";
+	cout << setw(13) << left << "NewBalance";
+	cout << "\n";
+
+	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
+	//you can change input argument of 'setprecision()' to see the effect
+	int i = 1;
+	while (loan > 0)
+	{
+		cout << fixed << setprecision(2);
+		cout << setw(13) << left << i;
+		cout << setw(13) << left << loan;
+		interest = (loan * interest_rate) / 100;
+		cout << setw(13) << left << interest;
+		loan = loan + interest;
+		cout << setw(13) << left << loan;
+		if (loan < payment)
+		{
+			payment = loan;
+			cout << setw(13) << left << payment;
+		}
+		else
+		{
+			cout << setw(13) << left << payment;
+		}
+		loan = loan - payment;
+		cout << setw(13) << left << loan;
+		cout << "\n";
+		i++;
+	}
+
+	return 0;
+}
---
+++ lab8_2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void printO(int N, int M)
+{
+	if (N <= 0 || M <= 0)
+	{
+		cout << "Invalid input";
+	}
+	else
+	{
+		for (int x = 0; x < N; x++)
+		{
+			for (int y = 0; y < M; y++)
+			{
+				cout << "O";
+			}
+			cout << "\n";
+		}
+	}
+}
+
+int main()
+{
+
+	printO(2, 2);
+	cout << "\n";
+
+	printO(3, 5);
+	cout << "\n";
+
+	printO(5, 3);
+	cout << "\n";
+
+	printO(0, 3);
+	cout << "\n";
+
+	printO(7, -1);
+	cout << "\n";
+
+	return 0;
+}
